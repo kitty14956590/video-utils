8dc5067fbd6bb1e0b7e578bc9461bdbdeeee5e0a
kitty14956590 <borbtrash@gmail.com>
2025-01-15 09:25:14 -0500

    :shrug:

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # video-utils
 Video processing scripts and stuff
+
+## modify "encoding/encoder.sh" and put whatever encoders you want to use in there
--- encoding/encoder.sh
+++ encoding/encoder.sh
@@ -6,21 +6,13 @@ fi
 
 ENCODING="$1"
 if [ "$ENCODING" == "hevc" ]; then
-	CHECK="$(ffmpeg -v error -encoders | grep hevc_nvenc)"
-	if [ "$CHECK" != "" ]; then
-		echo "hevc_nvenc"
-		exit 1
-	fi
 	echo "hevc"
+	exit 0
 fi
 
 if [ "$ENCODING" == "h264" ]; then
-	CHECK="$(ffmpeg -v error -encoders | grep h264_nvenc)"
-	if [ "$CHECK" != "" ]; then
-		echo "h264_nvenc"
-		exit 1
-	fi
 	echo "libx264"
+	exit 0
 fi
 
 echo "$ENCODING"
